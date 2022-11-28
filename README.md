4e48cfa90eed44827101169fcc76535eb7feba1b
syblackwell <syblackwell@anywhichway.com>
2022-11-28 09:19:32 -0800

    added footnotes, removed requirements for docs to be based on Markdown

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # autohelm
-A library to support standardized navigation in HTML files generated from Markdown with headings
+A library to support standardized table of contents and footnote navigation in HTML files
--- index.js
+++ index.js
@@ -1,136 +1,139 @@
-const patchHeadings = (node) => {
-    const footnotes = node.querySelector('.footnotes');
-    if(footnotes) {
-        const heading = document.createElement("h1");
-        heading.innerText = "Footnotes";
-        footnotes.parentElement.insertBefore(heading,footnotes);
+const toTOC = (dom,headings,toc,previousLevel,previousHeading) => {
+    const originalHeadings = [...headings],
+        ul = document.createElement("ol");
+    let previousLI;
+    while(headings.length>0) {
+        const heading = headings[0],
+            level = parseInt(heading.tagName.substring(1));
+        previousLevel ||= level;
+        if(level<previousLevel) {
+            return ul;
+        }
+        if(level>previousLevel) {
+            previousLI.appendChild(toTOC(dom,headings,toc,level,previousHeading));
+        } else {
+            previousLI = document.createElement("li");
+            previousLI.innerHTML = `<a href="#${heading.id}">${heading.innerHTML}</a>`;
+            previousLI.classList.add("autohelm-nav");
+            ul.appendChild(previousLI);
+            const headingEl = dom.querySelector("#"+heading.id),
+                navspan = document.createElement("span"),
+                tocspan = document.createElement('span'),
+                nextHeading = headings[1];
+            navspan.classList.add("autohelm-nav");
+            navspan.classList.add("autohelm-nav-up-down");
+            tocspan.classList.add("autohelm-nav");
+            tocspan.classList.add("autohelm-toc");
+            navspan.innerHTML = (previousHeading ? ` <a href="#${previousHeading.id}">&uarr;</a>` : '') + (nextHeading && !headingEl.classList.contains("toc") ?  ` <a href="#${nextHeading.id}">&darr;</a>` :'');
+            tocspan.innerHTML = `<a href="#${toc.id}">&#9783;</a> `;
+            previousHeading = headings.shift();
+            headingEl.insertBefore(tocspan,headingEl.firstChild);
+            headingEl.appendChild(navspan);
+        }
+        //previousLI = li;
     }
-    [...node.getElementsByClassName("data-footnote-backref")].forEach((a) => {
-        a.setAttribute("target","_self");
-    })
-    for(let i=1;i<10;i++) {
-        [...node.querySelectorAll("h"+i)].forEach((heading,i,headings) => {
-            if(!isSlotted(heading)) {
-                if (!heading.getAttribute("id")) {
-                    heading.setAttribute("id", heading.innerText.toLowerCase().replace(/\s/g, "-"))
-                }
-                heading.setAttribute("title",heading.innerText)
-                heading.classList.add("ah-heading");
+    return ul;
+}
+
+const buildTOC = ({tocSelector=".toc",dom = document.body}={}) => {
+    const tocEl = dom.querySelector(tocSelector);
+    if(!tocEl) {
+        throw new Error(`No TOC element found for "${tocSelector}`);
+    }
+    for(let i=0;i<=6;i++) {
+        [...dom.querySelectorAll("h"+i)].forEach((heading) => {
+            if(heading.id.length===0) {
+                let text =  heading.textContent;
+                [" ","/",".","&"].forEach((separator) => {
+                    text = text.split(separator).map((word) => word.toLowerCase()).join("-")
+                })
+                heading.setAttribute("id",text);
             }
-        });
+            heading.classList.add("autohelm-heading")
+        })
     }
-    const toc = node.querySelector("#toc")||node.querySelector("#table-of-contents");
-    let floatingToc;
-    if(toc) {
-        const footnotes = node.querySelector("#footnotes");
-        if(footnotes && !toc.nextElementSibling.querySelector('a[href="#footnotes"]')) {
-            const li = document.createElement("li");
-            li.classList.add("toc-item");
-            li.innerHTML = '<a href="#footnotes">Footnotes</a>';
-            toc.nextElementSibling.appendChild(li);
-        }
-        convertList(toc.nextElementSibling,"OL","toc-item");
-        floatingToc = toc.nextElementSibling.cloneNode(true);
-        const tocLink = document.createElement("li");
-        tocLink.setAttribute("value","0");
-        tocLink.innerHTML = `<a href="#${toc.id}" target="_self">${toc.innerText}</a>`;
-        floatingToc.insertBefore(tocLink,floatingToc.firstChild);
-        floatingToc.setAttribute("id","ah-floating-toc");
-        floatingToc.style.display = "none";
-        floatingToc.style.paddingTop = "5px";
-        floatingToc.style.paddingRight = "5px";
-        floatingToc.style.paddingBottom = "5px";
-        floatingToc.style.zIndex = 100;
-        floatingToc.style.opacity = 1;
-        floatingToc.style.backgroundColor = "white";
-        floatingToc.style.border = "1px solid gray";
-        floatingToc.style.maxHeight = "250px";
-        floatingToc.style.overflow = "auto";
-        floatingToc.style.position = "absolute";
-        node.appendChild(floatingToc);
+    const headings = [...dom.querySelectorAll(".autohelm-heading")];
+    while(!headings[0]?.classList.contains("toc") && headings.length>0) {
+        headings.shift();
     }
-    let tocOn;
-    [...node.querySelectorAll(".ah-heading")].forEach((heading,i,headings) => {
-        if(i>0) {
-            const previous = document.createElement("a");
-            previous.setAttribute("target","_self");
-            previous.setAttribute("href","#"+headings[i-1].id);
-            previous.setAttribute("title",headings[i-1].getAttribute("title"));
-            previous.classList.add("ah-nav");
-            previous.innerHTML = "&uarr;";
-            heading.appendChild(previous);
+    const toc = toTOC(dom,headings,tocEl);
+    tocEl.insertAdjacentElement("afterend",toc);
+}
+
+const buildFootnotes = ({dom = document.body,footnotesTitle = "Footnotes",footnotesLevel=1}={}) => {
+    const heading = document.createElement("h"+footnotesLevel);
+    heading.innerHTML = footnotesTitle;
+    dom.appendChild(heading);
+    const footnotes = [...dom.querySelectorAll(".autohelm-footnote")];
+    footnotes.forEach((footnote,i) => {
+        i++; // numbering starts at 1
+        const href = footnote.getAttribute("href"),
+            p = href ? dom.querySelector(href)  || document.createElement("p") : document.createElement("p");
+        p.id ||= (footnote.id || `autohelm-footnote${i}`);
+        footnote.id = `autohelm-footnote-ref${i}`;
+        footnote.removeAttribute("href");
+        const numbers = p.firstElementChild || document.createElement("span");
+        if(numbers!=p.firstElementChild) p.appendChild(numbers);
+        const backref = document.createElement("a");
+        backref.setAttribute("href","#"+footnote.id);
+        backref.innerHTML = i;
+        if(numbers.childNodes.length>0) {
+            numbers.appendChild(new Text(", "))
         }
-        if(i<headings.length-1) {
-            const next = document.createElement("a");
-            next.setAttribute("target","_self");
-            next.setAttribute("href","#"+headings[i+1].id);
-            next.setAttribute("title",headings[i+1].getAttribute("title"));
-            next.classList.add("ah-nav");
-            next.innerHTML = "&darr;";
-            heading.appendChild(next);
+        numbers.appendChild(backref);
+        if(p.children.length===1) {
+            p.appendChild(new Text(" "));
+            while(footnote.firstChild) {
+                p.appendChild(footnote.firstChild)
+            }
         }
-        if((tocOn || heading.id==="toc" || heading.id==="table-of-contents") && floatingToc) {
-            const toc = document.createElement("span");
-            toc.classList.add("ah-toc-icon");
-            toc.innerHTML = "&#9783";
-            toc.style.marginRight = ".25em";
-            heading.insertBefore(toc,heading.firstChild);
-        }
-        if(heading.id==="toc" || heading.id==="table-of-contents") {
-            tocOn = true;
+        footnote.innerHTML = `<a href="${href || '#'+p.id}">${i}</a>`;
+        if(!dom.querySelector("#"+p.id)) {
+            dom.appendChild(p)
         }
     })
 }
 
-const activate = (node) => {
-    [...node.querySelectorAll("li.toc-item a")].forEach((a) => {
-        a.setAttribute("target","_self");
-    })
-    node.addEventListener("click",(event) => {
-        const target = event.target;
-        if(target.classList.contains("ah-toc-icon")) {
-            const floatingToc = node.querySelector("#ah-floating-toc");
-            event.preventDefault();
-            event.stopImmediatePropagation();
-            const {x,y,height} = target.getBoundingClientRect(),
-                top =  (y+height+250+window.scrollY) > floatingToc.parentElement.clientHeight ? ((y - 250) - height)+window.scrollY:  (y+height+window.scrollY);
-            floatingToc.style.left = x+"px";
-            floatingToc.style.top = top+"px";
-            floatingToc.style.display = "block";
-        }
-    });
-    node.addEventListener("scroll",() => {
-        const floatingToc = node.querySelector("#ah-floating-toc");
-        if(floatingToc) floatingToc.style.display="none";
-    })
-    node.addEventListener("click",() => {
-        const floatingToc = node.querySelector("#ah-floating-toc");
-        if(floatingToc) floatingToc.style.display="none";
-    });
-    node.classList.add("ah-list"); // toc-list?
+const init = ({tocSelector=".toc",dom = document.body,footnotesTitle="Footnotes",footnotesLevel=1}={}) => {
+    buildFootnotes({dom,footnotesTitle,footnotesLevel});
+    buildTOC({tocSelector,dom});
 }
 
-const isSlotted = (node) => {
-    return node.tagName==="SLOT" || (node.parentElement && isSlotted(node.parentElement))
+const engage = () => {
+    let tocClone;
+    document.body.addEventListener("click",(event) => {
+        const tocEl = document.body.querySelector(".toc");
+        if(tocEl) {
+            const anchors = [...document.body.querySelectorAll(`.autohelm-toc a[href="#${tocEl.id}"]`)];
+            if(anchors.includes(event.target) && !tocEl.contains(event.target)) {
+                event.preventDefault();
+                const {top,left} = event.target.getBoundingClientRect();
+                if(!tocClone) {
+                    tocClone = tocEl.nextElementSibling.cloneNode(true);
+                    tocClone.classList.add("autohelm-toc-popup");
+                    tocClone.style.position = "absolute";
+                    tocClone.style.height = tocClone.style.maxHeight = "300px";
+                    tocClone.style.zIndex = 100;
+                    tocClone.style.background = "whitesmoke";
+                    tocClone.style.opacity = 1;
+                    tocClone.style.overflow = "auto";
+                    tocClone.classList.add("autohelm-toc-popup");
+                    tocClone.style.position = "absolute";
+                    document.body.appendChild(tocClone);
+                }
+                tocClone ||= tocEl.nextElementSibling.cloneNode(true);
+                tocClone.style.height = tocClone.style.maxHeight = "300px";
+                tocClone.style.top = top+300 > window.innerHeight ? top+window.scrollY-300+"px" : top+window.scrollY+"px";
+                tocClone.style.left =left+"px";
+                tocClone.style.display = "block";
+            } else {
+                if(tocClone) {
+                    tocClone.style.display = "none";
+                }
+            }
+        }
+    })
 }
 
-const convertList = (list,to=list.tagName==="OL" ? "UL" : "OL",className) => {
-    [...list.children].forEach((child) => {
-        convertList(child,to,className);
-    });
-    if(["OL","UL"].includes(list.tagName) && list.tagName!==to) {
-        const replacement = document.createElement(to);
-        if(className) {
-            replacement.classList.add(className);
-            [...list.querySelectorAll("li")].forEach((li) => {
-                li.classList.add(className);
-            })
-        }
-        while(list.firstChild) replacement.appendChild(list.firstChild);
-        list.replaceWith(replacement)
-    }
-}
-const AutoHelm = {
-    patchHeadings,activate
-}
-export {AutoHelm as default}
+
+export {buildTOC,buildFootnotes,init,engage}
